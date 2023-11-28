9e37d8ab4936c2feec07eae6bafb8bd188c0b99e
Franklin Souza <fraank@riseup.net>
2023-11-28 20:47:25 -0300

    Commit automático no repositório arch-packages-wayland!

---
+++ arch-packages-wayland.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Franklin Souza
+# @FranklinTech
+
+clear
+sudo pacman -S hyprland \
+  wayland \
+  firefox \
+  git \
+  kitty \
+  dunst \
+  pavucontrol \
+  pipewire \
+  pipewire-alsa \
+  pipewire-pulse \
+  polkit-gnome \
+  ttf-font-awesome \
+  waybar \
+  wireplumber \
+  wofi \
+  xdg-desktop-portal-hyprland \
+  bitwarden \
+  wine-staging \
+  qemu \
+  gucharmap \
+  catimg \
+  mangohud \
+  lib32-mangohud \
+  gamemode \
+  lib32-gamemode \
+  spotifyd \
+  streamlink \
+  dosfstools \
+  npm \
+  swaylock \
+  swaybg \
+  grim \
+  slurp \
+  wl-clipboard --noconfirm
