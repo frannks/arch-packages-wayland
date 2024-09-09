aadcdcf6af9723c50042d436b5df7e03a6820662
Franklin Souza <fraank@riseup.net>
2024-09-09 16:52:40 -0300

    Commit automático no repositório arch-packages-wayland!

--- arch-packages-wayland.sh
+++ arch-packages-wayland.sh
@@ -8,29 +8,12 @@ sudo pacman -S hyprland \
   wayland \
   xorg-xwayland \
   xf86-video-amdgpu \
-  firefox-i18n-pt-br \
-  git \
   kitty \
-  dunst \
-  pavucontrol \
-  pipewire \
-  pipewire-alsa \
-  pipewire-pulse \
   waybar \
-  wofi \
   wireplumber \
   xdg-desktop-portal-hyprland \
-  bitwarden \
-  wine-staging \
-  qemu \
-  gucharmap \
-  catimg \
-  mangohud \
-  lib32-mangohud \
-  gamescope \
-  lib32-gamemode \
-  dosfstools \
-  npm \
+  base-devel \
+  polkit-gnome \
   hyprpaper \
   grim \
   slurp \
@@ -38,7 +21,4 @@ sudo pacman -S hyprland \
   usbutils \
   zathura \
   zathura-pdf-poppler \
-  thunderbird \
-  thunderbird-dark-reader \
-  thunderbird-i18n-pt-br \
   wl-clipboard --noconfirm
